248c8d63cf6df4daa82d5585ae8d75b8355b651e
nazim117 <61702781+nazim117@users.noreply.github.com>
2024-05-11 17:17:37 +0200

    still testing pipeline

--- src/main/java/org/example/individualbackend/business/GeneralExceptions/InvalidCredentialsException.java
+++ src/main/java/org/example/individualbackend/business/GeneralExceptions/InvalidCredentialsException.java
@@ -2,7 +2,6 @@ package org.example.individualbackend.business.GeneralExceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
-//ADAWDAWDAWD
 public class InvalidCredentialsException extends ResponseStatusException {
     public InvalidCredentialsException() {
         super(HttpStatus.BAD_REQUEST, "INVALID_CREDENTIALS");
