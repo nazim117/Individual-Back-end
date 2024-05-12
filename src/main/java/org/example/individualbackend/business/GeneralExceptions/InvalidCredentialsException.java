0c3905fc3ec28f031603b2ae868c64b40c8e5c58
nazim117 <61702781+nazim117@users.noreply.github.com>
2024-05-12 22:16:50 +0200

    testing elasticsearch

--- src/main/java/org/example/individualbackend/business/GeneralExceptions/InvalidCredentialsException.java
+++ src/main/java/org/example/individualbackend/business/GeneralExceptions/InvalidCredentialsException.java
@@ -2,6 +2,7 @@ package org.example.individualbackend.business.GeneralExceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+//added stuff
 public class InvalidCredentialsException extends ResponseStatusException {
     public InvalidCredentialsException() {
         super(HttpStatus.BAD_REQUEST, "INVALID_CREDENTIALS");
