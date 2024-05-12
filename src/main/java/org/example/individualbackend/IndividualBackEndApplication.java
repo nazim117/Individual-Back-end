e8a82b9a8b5c08bb93e2cb8230a30a151ec54026
nazim117 <61702781+nazim117@users.noreply.github.com>
2024-05-12 21:52:55 +0200

    testing elasticsearch

--- src/main/java/org/example/individualbackend/IndividualBackEndApplication.java
+++ src/main/java/org/example/individualbackend/IndividualBackEndApplication.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class IndividualBackEndApplication {
-//test
     public static void main(String[] args) {
         SpringApplication.run(IndividualBackEndApplication.class, args);
     }
-
 }
