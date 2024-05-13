37ed9073fa002a0afa2cbceb6e42a82b238c9115
nazim117 <61702781+nazim117@users.noreply.github.com>
2024-05-13 09:47:27 +0200

    testing jacoco

--- src/main/java/org/example/individualbackend/Utils/TicketGenerator.java
+++ src/main/java/org/example/individualbackend/Utils/TicketGenerator.java
@@ -1,26 +1,21 @@
 package org.example.individualbackend.Utils;
 
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.example.individualbackend.persistance.TicketRepo;
 import org.example.individualbackend.persistance.entity.TicketEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 
-//TODO: MAKE SOME IMPROVEMENTS ON THE PRICE GENERATION (make the generation more precise)
+//TODO: MAKE SOME IMPROVEMENTS ON THE PRICE GENERATION (make the generation more accurate based on seat and row number)
 @NoArgsConstructor
 public enum TicketGenerator {
     INSTANCE;
 
     private static final Random random = new Random();
     private static final double BASE_PRICE = 30.0;
-    private static final double PRICE_VARIATION = 20.0;
+    private static final double PRICE_VARIATION = 25.0;
 
     public List<TicketEntity> generateTicket(int rowCount, int seatsPerRow) {
         List<TicketEntity> tickets = new ArrayList<>();
@@ -45,7 +40,7 @@ public enum TicketGenerator {
     }
 
     private double generatePrice(int rowNumber) {
-        double price =  BASE_PRICE + (rowNumber * PRICE_VARIATION) + (random.nextDouble() * 10.0 - 5.0);
+        double price =  BASE_PRICE + (rowNumber * PRICE_VARIATION) + (random.nextDouble() * 10.0);
         return Math.round(price * 100.0) / 100.0;
     }
 }
