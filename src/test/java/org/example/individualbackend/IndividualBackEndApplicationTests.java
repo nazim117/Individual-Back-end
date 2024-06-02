c732752e077abbc8b6dabe9b884cc5d06f0e1af4
nazim117 <61702781+nazim117@users.noreply.github.com>
2024-06-02 18:48:50 +0200

    fixed unit tests

--- src/test/java/org/example/individualbackend/IndividualBackEndApplicationTests.java
+++ src/test/java/org/example/individualbackend/IndividualBackEndApplicationTests.java
@@ -2,18 +2,24 @@ package org.example.individualbackend;
 
 import org.example.individualbackend.external_api.UnirestWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class IndividualBackEndApplicationTests {
     @MockBean
-    private UnirestWrapper unirestWrapper;
+    private UnirestWrapper unirest;
+    @Value("${rapidApi.key}")
+    private String apiKey;
     @Test
     void contextLoads() {
         assertTrue(true, "Context loaded successfully");
+        assertNotNull(apiKey);
+        System.out.println(apiKey);
     }
 
 }
